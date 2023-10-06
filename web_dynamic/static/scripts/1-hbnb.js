100cb57d160e45ed641c9f12b9e8d89fb4386f0c
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-10-06 13:56:44 +0300

    add solutions to task 2

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  console.log('jquery script loaded');
+  $('input:checkbox:checked').prop('checked', false);
+  const data = {};
+  $('input:checkbox').on('click', function () {
+    if ($(this).prop('checked') === true) {
+      data[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else if ($(this).attr('data-id') in data) {
+      delete data[$(this).attr('data-id')];
+    }
+    if (Object.values(data).length > 0) {
+      $('div.amenities h4').text(Object.values(data).join(', '));
+    } else {
+      $('div.amenities h4').html('&nbsp;');
+    }
+  });
+});
