993c26b0c74003301a3a0dea26313b007dc0eef3
Alex Jarabi <jarabialex@gmail.com>
2023-10-08 02:57:32 +0300

    Filter places by amenity

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,81 @@
+$(document).ready(function () {
+  // Check API status
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (res) {
+    if (res.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+
+  // Get all places
+  $.ajax({
+    type: 'POST',
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    contentType: 'application/json',
+    data: '{}',
+    dataType: 'json',
+    success: fetchPlaces
+  });
+
+  $('input:checkbox:checked').prop('checked', false);
+
+  const amenities = {};
+
+  // Get selected amenities
+  $('input:checkbox').on('click', function () {
+    if ($(this).prop('checked') === true) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else if ($(this).attr('data-id') in amenities) {
+      delete amenities[$(this).attr('data-id')];
+    }
+    if (Object.values(amenities).length > 0) {
+      $('div.amenities h4').text(Object.values(amenities).join(', '));
+    } else {
+      $('div.amenities h4').html('&nbsp;');
+    }
+  });
+
+  // Filter places by amenities
+  $('button').on('click', function () {
+    $.ajax({
+      type: 'POST',
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      contentType: 'application/json',
+      data: JSON.stringify({ amenities: Object.keys(amenities) }),
+      dataType: 'json',
+      success: fetchPlaces
+    });
+  });
+});
+
+function fetchPlaces (data) {
+  $('section.places').empty();
+  $('section.places').append(data.map(place => {
+    return `<article>
+              <div class="title_box">
+                <h2>${place.name}</h2>
+                <div class="price_by_night">
+                  ${place.price_by_night}
+                </div>
+              </div>
+              <div class="information">
+                <div class="max_guest">
+                  ${place.max_guest} Guests
+                </div>
+                <div class="number_rooms">
+                  ${place.number_rooms} Bedrooms
+                </div>
+                <div class="number_bathrooms">
+                  ${place.number_bathrooms} Bathrooms
+                </div>
+              </div>
+              <div class="user">
+                <b>Owner:</b> firstName lastName
+              </div>
+              <div class="description">
+                ${place.description}
+              </div>
+            </article>`;
+  }));
+}
